db881edd470c16a041c63b8e39b404918dba09bd
Andrew Schmittat <andyschmittat@gmail.com>
2017-02-06 13:31:10 -0500

    webcrawler idk
    robots.txt stuff

--- WebCrawler.java
+++ WebCrawler.java
@@ -14,9 +14,9 @@ public class WebCrawler
     	websites = new LinkedList();
     	try
     	{
-    		System.out.println("pls work");
+    		//System.out.println("pls work");
     		crawl(branchLimit,new URL(URLString));
-    		System.out.println("pls work");
+    		//System.out.println("pls work");
     	}
     	catch(Exception e)
     	{
@@ -36,10 +36,12 @@ public class WebCrawler
     
     private void crawl(int n,URL url)
     {
-    	System.out.println("n: " + n + " url: " + url.toString());
+    	//System.out.println("n: " + n + " url: " + url.toString());
+    	
+    	
     	if(n > 0 && websites.size() < sizeLimit)
     	{
-    		System.out.println("pls workweewesdf");
+    		//System.out.println("pls workweewesdf");
     		InputStream in = null;
     		DataInputStream dataIn;
     		String line;
@@ -57,8 +59,21 @@ public class WebCrawler
 	    				{
 	    					i += 6;
 	    					URL subURL = new URL(line.substring(i,line.indexOf(line.charAt(i-1),i)));
-	    					System.out.println(subURL.toString());
-	    					if(!websites.contains(subURL))
+	    					//System.out.println(subURL.toString());
+	    					boolean allow = true;
+	    					try
+	    					{
+	    						int j;
+		    					String robotLine;
+		    					DataInputStream robot = new DataInputStream(new BufferedInputStream(new URL(subURL.toString() + "/robots.txt").openStream()));
+		    					while((robotLine = robot.readLine()) != null)
+		    						if((j = robotLine.indexOf("Disallow:")) != -1 && robotLine.substring(9).equals(" "))
+		    							allow = false;
+	    					}
+	    					catch(Exception e){}
+	    					
+	    					
+	    					if(!websites.contains(subURL) && allow)
 	    					{
 	    						websites.add(subURL);
 	    						crawl(n-1,subURL);
@@ -67,7 +82,7 @@ public class WebCrawler
     				}
     				catch(MalformedURLException mue)
     				{
-    					System.out.println("mue error");
+    					//System.out.println("mue error");
     				}
     				
     			}
@@ -75,7 +90,7 @@ public class WebCrawler
     		}
     		catch(IOException ioe)
     		{
-    			ioe.printStackTrace();
+    			//ioe.printStackTrace();
     		}
     		finally
     		{
@@ -90,10 +105,10 @@ public class WebCrawler
     
     public static void main(String[]args)
     {
-    	WebCrawler crawler = new WebCrawler(100,5,"https://en.wikipedia.org/wiki/Main_Page");
-    	System.out.println("pls wor4k");
+    	WebCrawler crawler = new WebCrawler(100,5,"https://amirqualitymeats.com");
+    	//System.out.println("pls wor4k");
     	URL[] siteArray = crawler.getArray();
-    	System.out.println("pls work" + siteArray.length);
+    	//System.out.println("pls work" + siteArray.length);
     	for(int i=0;i < siteArray.length;i++)
     		System.out.println(i + " " + siteArray[i].toString());
     }
